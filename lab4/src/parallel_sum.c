4a620c8bda429e9158ddf9f5b42af126b0371602
tatianasergeeva <8170969@edu.miet.ru>
2020-04-21 18:28:44 +0000

    task 5

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -1,8 +1,10 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include <pthread.h>
+#include <getopt.h>
 
 struct SumArgs {
   int *array;
@@ -22,17 +24,73 @@ void *ThreadSum(void *args) {
 }
 
 int main(int argc, char **argv) {
-  /*
-   *  TODO:
-   *  threads_num by command line arguments
-   *  array_size by command line arguments
-   *	seed by command line arguments
-   */
 
   uint32_t threads_num = 0;
   uint32_t array_size = 0;
   uint32_t seed = 0;
   pthread_t threads[threads_num];
+
+   while (true) {
+    int current_optind = optind ? optind : 1;
+
+    static struct option options[] = {{"seed", required_argument, 0, 0},
+                                      {"array_size", required_argument, 0, 0},
+                                      {"threads_num", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "f", options, &option_index);
+
+    if (c == -1) break;
+
+    switch (c) {
+      case 0:
+        switch (option_index) {
+          case 0:
+            seed = atoi(optarg);
+              if (seed <= 0) {
+                printf("seed is a positive number\n");
+                 return 1;
+                }
+            break;
+          case 1:
+            array_size = atoi(optarg);
+             if (array_size <= 0) {
+                printf("array_size is a positive number\n");
+                 return 1;
+                }
+            break;
+          case 2:
+            threads_num = atoi(optarg);
+             if (threads_num <= 0) {
+                printf("threads_num is a positive number\n");
+                 return 1;
+                }
+            break;
+
+          defalut:
+            printf("Index %d is out of options\n", option_index);
+        }
+        break;
+
+      case '?':
+        break;
+
+      default:
+        printf("getopt returned character code 0%o?\n", c);
+    }
+  }
+
+  if (optind < argc) {
+    printf("Has at least one no option argument\n");
+    return 1;
+  }
+
+  if (seed == 0 || array_size == 0 || threads_num == 0) {
+    printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" \n",
+           argv[0]);
+    return 1;
+  }
 
   /*
    * TODO:
@@ -41,6 +99,8 @@ int main(int argc, char **argv) {
    */
 
   int *array = malloc(sizeof(int) * array_size);
+
+  
 
   struct SumArgs args[threads_num];
   for (uint32_t i = 0; i < threads_num; i++) {
